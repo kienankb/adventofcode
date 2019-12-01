07489694bde64a118de60778f16867ad97cea0c6
Kienan Knight-Boehm <kienan@kienankb.com>
2019-12-01 15:45:19 -0800

    Day 01, part 02

--- 2019/01/solution.py
+++ 2019/01/solution.py
@@ -1,11 +1,24 @@
 import math
 
+def getFuelForMass(mass):
+    fuel = math.floor(int(mass)/3)-2
+    return fuel if fuel > 0 else 0
+
 def part1():
     totalFuel = 0
     for moduleMass in open('01-input.txt'):
-        fuel = math.floor(int(moduleMass)/3)-2
-        totalFuel += fuel
+        totalFuel += getFuelForMass(moduleMass)
+    return totalFuel
+
+def part2():
+    totalFuel = 0
+    for moduleMass in open('01-input.txt'):
+        fuel = getFuelForMass(moduleMass)
+        while fuel > 0:
+            totalFuel += fuel
+            fuel = getFuelForMass(fuel)
     return totalFuel
 
 if __name__ == '__main__':
-    print(part1())
+    print('Part 1:', part1())
+    print('Part 2:', part2())
