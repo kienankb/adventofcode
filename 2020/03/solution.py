efd69b98473ba3d074a1e4a9ad12f0ea6c082774
Kienan Knight-Boehm <kienan@kienankb.com>
2020-12-06 00:39:05 -0800

    2020, day 3, part 2

--- 2020/03/solution.py
+++ 2020/03/solution.py
@@ -1,16 +1,18 @@
-def part1():
+def part1(over=3, down=1):
     with open('./03-input.txt', 'r') as inFile:
         terrain = [line.strip() for line in inFile]
+    level = 0
     encounteredTrees = 0
     horizontalPosition = 0
-    for level in terrain:
-        if level[horizontalPosition % len(level)] == '#':
+    while level < len(terrain):
+        if terrain[level][horizontalPosition % len(terrain[level])] == '#':
             encounteredTrees += 1
-        horizontalPosition += 3
+        horizontalPosition += over
+        level += down
     return encounteredTrees
 
 def part2():
-    pass
+    return part1(over=1)*part1()*part1(over=5)*part1(over=7)*part1(over=1,down=2)
 
 print(part1())
 print(part2())
